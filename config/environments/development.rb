de5d0c00ed4ae9ecfc796056bd63508fcb7d6c3f
anne castrillon <>
2020-09-15 12:24:47 -0700

    allow all origins

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,9 +48,11 @@ ActionMailer::Base.smtp_settings = {
   :authentication       => "plain"
 } 
 
+
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4200'
+    origins '*'
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
