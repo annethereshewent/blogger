4c14c78e94cf40033a8084c6777fdc06ab4f1eef
anne castrillon <>
2020-09-16 21:17:34 -0700

    minor change!

--- app/lib/custom_token_response.rb
+++ app/lib/custom_token_response.rb
@@ -2,12 +2,8 @@ module CustomTokenResponse
   def body
     user = User.find(@token.resource_owner_id)
 
-    friends = user.friends.where('(sender = ?) or (friendships.accepted = true)', user.id)
-    formatted_posts = user.get_json_posts(friends)
-
     additional_data = {
         user: user.render_hash_user(),
-        posts: formatted_posts
     }
 
     # call original `#body` method and merge its result with the additional data hash
